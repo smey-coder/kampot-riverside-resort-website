c7a848f4572d71eff733e324debecec1867361cc
Your Name <sanreaksmey01@gmail.com>
2026-06-15 08:16:44 +0700

    Reaksmey

--- js/main.js
+++ js/main.js
@@ -75,3 +75,19 @@ document.querySelectorAll("a").forEach(link => {
         }
     });
 });
+// SLIDER
+const slides = document.querySelectorAll('.slide');
+let current = 0;
+
+function showSlide(index){
+    slides.forEach(slide => slide.classList.remove('active'));
+    slides[index].classList.add('active');
+}
+
+setInterval(() => {
+    current++;
+    if(current >= slides.length){
+        current = 0;
+    }
+    showSlide(current);
+}, 5000);
